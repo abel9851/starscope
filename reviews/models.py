36e31aad4489e6135d4c10dc5fbbd735012e10c1
Heejun Shin <abel9851@gmail.com>
2021-02-24 11:51:00 +0900

    Set reviews app

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Review(core_models.TimeStampedModel):
+
+    """ Review Model Definition """
+
+    review = models.TextField(verbose_name="レビュー")
+    accuracy = models.IntegerField(verbose_name="正確性")
+    location = models.IntegerField(verbose_name="位置")
+    cleanliness = models.IntegerField(verbose_name="淸潔")
+    satisfaction = models.IntegerField(verbose_name="満足度")
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    place = models.ForeignKey("places.Place", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.review} - {self.place}"
