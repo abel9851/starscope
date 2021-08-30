670dbf8594ea04263016acb92f1e643fdf0a24e5
Heejun Shin <abel9851@gmail.com>
2021-08-30 20:48:53 +0900

    fix create review function

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = "reviews"
 
 urlpatterns = [
-    path("<int:place>/", views.form_review, name="form"),
     path("create/<int:place>/", views.create_review, name="create"),
     path("update/<int:place>/<int:comment>", views.update_review, name="update"),
     path("delete/<int:place>/<int:comment>", views.delete_review, name="delete"),
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,5 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.views.generic import View
 from django.contrib.auth.decorators import login_required
 from places import models as place_models
 from . import models as review_models
@@ -8,26 +7,26 @@ from . import forms
 
 
 @login_required
-def form_review(request, place):
-    place = place_models.Place.objects.get_or_none(pk=place)
-    form = forms.CreateReviewForm()
-    return render(request, "reviews/form.html", {"form": form, "place": place})
-
-
-@login_required
 def create_review(request, place):
+
+    if request.method == "GET":
+        place = get_object_or_404(place_models.Place, pk=place)
+        form = forms.CreateReviewForm()
+
     if request.method == "POST":
-        form = forms.CreateReviewForm(request.POST)
         place = place_models.Place.objects.get_or_none(pk=place)
         if not place:
             return redirect(reverse("core:home"))
+        form = forms.CreateReviewForm(request.POST)
         if form.is_valid():
             review = form.save()
             review.place = place
             review.user = request.user
             review.save()
-            messages.success(request, "Place reviewed")
+            messages.success(request, "place reviewed")
             return redirect(reverse("places:detail", kwargs={"pk": place.pk}))
+
+    return render(request, "reviews/create.html", {"form": form, "place": place})
 
 
 @login_required
